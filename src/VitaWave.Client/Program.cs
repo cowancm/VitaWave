220ad89a53886d7c91ea998b577fb302aa949bf8
chris cowan <cowancm2003@gmail.com>
2025-04-10 21:49:49 -0500

    we got a chart view nowgit add -Agit add -Agit add -Agit add -A

--- src/VitaWave.Client/Program.cs
+++ src/VitaWave.Client/Program.cs
@@ -2,10 +2,13 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using VitaWave.Client;
 
+System.Console.Write("Foo bar!");
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
 
 await builder.Build().RunAsync();
