55431402cce2dca0964ac4220420a392ac082042
chris cowan <cowancm2003@gmail.com>
2025-04-15 21:44:28 -0500

    refactors

---
+++ src/VitaWave.Common/Interfaces/ITargetUpdateService.cs
@@ -0,0 +1,16 @@
+﻿using Common.PreProcessed.TLVs;
+using Common.Visualizer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Interfaces
+{
+    public interface ITargetUpdateService
+    {
+        event EventHandler<List<Target>>? TargetsUpdated;
+        void UpdateTargets(List<VisualizerTarget> newTargets);
+    }
+}
