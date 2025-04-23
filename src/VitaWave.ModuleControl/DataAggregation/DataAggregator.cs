33958a5b4c6396e58764ac726e5d6893cccdcf0c
chris <cowancm2003@gmail.com>
2025-04-23 15:23:33 -0500

    add some stuff to data aggregator

--- src/VitaWave.ModuleControl/DataAggregation/DataAggregator.cs
+++ src/VitaWave.ModuleControl/DataAggregation/DataAggregator.cs
@@ -9,5 +9,7 @@ namespace VitaWave.ModuleControl.DataAggregation
 {
     internal class DataAggregator : IDataAggregator
     {
+
+
     }
 }
---
+++ src/VitaWave.ModuleControl/DataAggregation/EntityConstants.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VitaWave.ModuleControl.DataAggregation
+{
+
+    //later will prolly be proto, for now, these will be defined here 
+    internal static class EntityConstants
+    {
+
+        public static Dictionary<EntityStatus, EntityActionType> _actionDict =>
+            new Dictionary<EntityStatus, EntityActionType>()
+            {
+                {EntityStatus.SITTING,   EntityActionType.STATIC},
+                {EntityStatus.STANDING,  EntityActionType.STATIC},
+                {EntityStatus.ACTIVE,    EntityActionType.ACTIVE}
+            };
+
+
+        public static EntityActionType? GetActionTypeByEntityStatus(EntityStatus status)
+        {
+            if (_actionDict.TryGetValue(status, out var action))
+            {
+                return action;
+            }
+
+            else return null;
+        }
+    }
+
+    public enum EntityStatus
+    {
+        SITTING,
+        STANDING,
+        ACTIVE
+    }
+
+    public enum EntityActionType
+    {
+        STATIC,
+        ACTIVE
+    }
+
+
+}
