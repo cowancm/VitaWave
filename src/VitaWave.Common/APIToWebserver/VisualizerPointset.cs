5e121c6dc12b5ce4f15e4e1ef198fb3022348869
chris <cowancm2003@gmail.com>
2025-05-02 11:28:33 -0500

    more server stuff

---
+++ src/VitaWave.Common/APIToWebserver/VisualizerPointset.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace VitaWave.Common.APIToWebserver
+{
+    public class PersonPoint
+    {
+        public const string UNKNOWN_STATUS = "Unknown";
+
+        public required double X { get; set; }
+        public required double Y { get; set; }
+        public required uint TID { get; set; }
+        public string Status { get; set; } = UNKNOWN_STATUS;
+    }
+}
