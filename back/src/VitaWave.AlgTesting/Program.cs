a6c4b3e3ee0b62babbbb2d418cf9121a414e9ac0
chris cowan <cowancm2003@gmail.com>
2025-09-30 13:06:21 -0500

    add alg testing app

---
+++ back/src/VitaWave.AlgTesting/Program.cs
@@ -0,0 +1,38 @@
+﻿using Serilog;
+using VitaWave.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ModuleControl
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            try
+            {
+                Log.Logger = new LoggerConfiguration()
+                    .MinimumLevel.Information()
+                    .WriteTo.Console()
+                    .CreateLogger();
+
+                await Host.CreateDefaultBuilder(args)
+                    .ConfigureServices((context, services) =>
+                    {
+                        services.AddSingleton<DataProcessor>();
+                        services.AddSingleton<DataFacilitator>();
+                    })
+                    .RunConsoleAsync();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+    }
+}
---
+++ back/src/VitaWave.AlgTesting/ResultConsoleOut.cs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VitaWave.Data;
+
+namespace AlgTesting
+{
+    public class ResultConsoleOut
+    {
+        public ResultConsoleOut(DataProcessor dataProcessor) 
+        {
+            dataProcessor.EventRaise += OnNewEvent;
+        }
+
+        private void OnNewEvent(object? sender, VitaWave.Common.ResultEvent e)
+        {
+            var messageBody = $"The following event took place \"{e.Event}\" at ${e.DateTimeString}.";
+        }
+    }
+}
