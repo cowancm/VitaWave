0afaab296ddea18d9e40d5d2212fe199ec75b968
chris cowan <cowancm2003@gmail.com>
2025-10-14 13:56:46 -0500

    push

--- front/main.js
+++ front/main.js
@@ -1,13 +1,24 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1000,
     height: 800,
-    webPreferences: { nodeIntegration: true, contextIsolation: false }
+    minWidth: 600,    // optional minimum width
+    minHeight: 400,   // optional minimum height
+    resizable: true,  // allow resizing
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
   });
-  win.loadFile('index.html');
+
+  win.loadFile(path.join(__dirname, 'index.html'));
 }
 
 app.whenReady().then(createWindow);
